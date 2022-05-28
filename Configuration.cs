271aee0dde6d59d1a69b8df5611d62746ca20800
Cara <git@caraxian.com>
2022-05-28 11:38:36 +0930

    Profiles

--- Configuration.cs
+++ Configuration.cs
@@ -1,22 +1,66 @@
-﻿using Dalamud.Configuration;
-using Dalamud.Plugin;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace CloneSession
 {
     [Serializable]
-    public class Configuration : IPluginConfiguration
+    public class Configuration
     {
-        public int Version { get; set; } = 0;
+        public List<ProfileConfig> Profiles = new();
+        
+        public string[] DalamudStreams = { "release", "stg", "net5" };
+        public bool FreeMutex = false;
+        public int DefaultProfileIndex;
 
+        [JsonIgnore]
+        public ProfileConfig DefaultProfile {
+            get {
+                if (DefaultProfileIndex >= Profiles.Count) DefaultProfileIndex = 0;
+                if (Profiles.Count == 0) Profiles.Add(new ProfileConfig() { ProfileName = "Default" });
+                return Profiles[DefaultProfileIndex];
+            }
+        }
+
+
+        public void Save() {
+            Save(this);
+        }
+
+        public static void Save(Configuration config) {
+            var configFile = new FileInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "CloneSession.json"));
+            
+            File.WriteAllText(configFile.FullName, JsonConvert.SerializeObject((object) config, Formatting.Indented, new JsonSerializerSettings() {
+                TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple,
+                TypeNameHandling = TypeNameHandling.None
+            }));
+        }
+
+        public static Configuration Load() {
+            var configFile = new FileInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "CloneSession.json"));
+            if (!configFile.Exists) return new Configuration();
+            return JsonConvert.DeserializeObject<Configuration>(File.ReadAllText(configFile.FullName), new JsonSerializerSettings()
+            {
+                TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple,
+                TypeNameHandling = TypeNameHandling.None
+            }) ?? new Configuration();
+        }
+        
+        
+    }
+
+    public class ProfileConfig {
+        public string ProfileName = string.Empty;
         public string UserPath = string.Empty;
         public string DalamudPath = string.Empty;
         public string GamePath = string.Empty;
-        public string[] DalamudStreams = { "release", "stg", "net5" };
         public string SelectedDalamudStream = "release";
 
-        public bool AutoClone = false;
-        public bool FreeMutex = false;
-
+        public override bool Equals(object? obj) {
+            if (obj is not ProfileConfig pCfg) return false;
+            return UserPath == pCfg.UserPath && DalamudPath == pCfg.DalamudPath && GamePath == pCfg.GamePath && SelectedDalamudStream == pCfg.SelectedDalamudStream;
+        }
     }
+    
 }
--- Plugin.cs
+++ Plugin.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Net;
+using System.Numerics;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
@@ -35,8 +36,8 @@ namespace CloneSession
         public static GameArguments GameArguments => GameArguments.Instance;
 
         public Plugin() {
-            this.Config = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
-
+            this.Config = Configuration.Load();
+            
             CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
             {
                 HelpMessage = "A useful message to display in /xlhelp"
@@ -50,19 +51,9 @@ namespace CloneSession
             showConfig = true;
 #endif
 
-            
-            if (Config.AutoClone) {
-                if (MutexCount() < 2) {
-                    CloneSession();
-                }
-            }
-
             if (Config.FreeMutex) {
                 ClearMutex();
             }
-            
-            
-            
         }
 
         public void Dispose() {
@@ -99,52 +90,131 @@ namespace CloneSession
                 ToggleConfigUI();
                 return;
             }
-
+            
             if (string.IsNullOrWhiteSpace(args)) {
-                CloneSession();
+                CloneSession(Config.DefaultProfile);
+            }
 
+            foreach (var p in Config.Profiles) {
+                if (p.ProfileName.Equals(args, StringComparison.InvariantCultureIgnoreCase)) {
+                    CloneSession(p);
+                    return;
+                }
             }
             
         }
 
+        private ProfileConfig? deletedItem;
+        
         private void DrawUI() {
             if (showConfig) {
                 if (ImGui.Begin($"{Name} Config", ref showConfig))
                 {
                     var hasChanged = false;
-                    hasChanged |= ImGui.InputTextWithHint("Custom User Path", GameArguments.UserPath, ref Config.UserPath, 1024);
-                    hasChanged |= ImGui.InputTextWithHint("Custom Dalamud Path", Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "XIVLauncher"), ref Config.DalamudPath, 1024);
-                    hasChanged |= ImGui.InputTextWithHint("Custom Game Path", GameArguments.GamePath, ref Config.GamePath, 1024);
                     
-                    hasChanged |= ImGui.Checkbox("Automatically Clone on Startup", ref Config.AutoClone);
                     hasChanged |= ImGui.Checkbox("Remove game instance limit", ref Config.FreeMutex);
                     
-                    if (ImGui.BeginCombo("Dalamud Stream", Config.SelectedDalamudStream)) {
-                        foreach (var s in Config.DalamudStreams) {
-                            if (ImGui.Selectable(s)) {
-                                Config.SelectedDalamudStream = s;
-                                hasChanged = true;
+                    ImGui.Separator();
+                    
+                    if (ImGui.BeginTabBar("profileTabs")) {
+
+                        var anyTabOpen = false;
+                        
+                        int i;
+                        for (i = 0; i < Config.Profiles.Count; i++) {
+                            var p = Config.Profiles[i];
+                            var n = string.IsNullOrEmpty(p.ProfileName) ? $"Profile {i}" : p.ProfileName;
+                            var isDefault = Config.DefaultProfileIndex == i;
+                            ImGui.PushStyleColor(ImGuiCol.Text, isDefault ? 0xFF00FFFF : 0xFFFFFFFF);
+                            
+                            var tabOpen = ImGui.BeginTabItem($"{n}###profile#{i}");
+                            anyTabOpen |= tabOpen;
+                            ImGui.PopStyleColor();
+                            if (tabOpen) {
+                                
+                                if (ImGui.Checkbox("Default Profile", ref isDefault)) {
+                                    hasChanged = true;
+                                    if (isDefault) Config.DefaultProfileIndex = i;
+                                }
+                                ImGui.SameLine();
+                                ImGui.Dummy(new Vector2(50, 1));
+                                ImGui.SameLine();
+                                if (ImGui.Button("Copy Profile")) {
+                                    Config.Profiles.Add(new ProfileConfig() {
+                                        DalamudPath = p.DalamudPath,
+                                        ProfileName = p.ProfileName,
+                                        GamePath = p.GamePath,
+                                        SelectedDalamudStream = p.SelectedDalamudStream,
+                                        UserPath = p.UserPath
+                                    });
+                                }
+                                if (!isDefault) {
+                                    ImGui.SameLine();
+                                    if (ImGui.Button("Delete Profile")) {
+                                        hasChanged = true;
+                                        deletedItem = p;
+                                        Config.Profiles.RemoveAt(i);
+                                        if (Config.DefaultProfileIndex > i) Config.DefaultProfileIndex--;
+                                        break;
+                                    }
+                                }
+                                
+                                
+                                hasChanged |= ImGui.InputTextWithHint("Profile Name", n, ref p.ProfileName, 1024);
+                                hasChanged |= ImGui.InputTextWithHint("Custom User Path", GameArguments.UserPath, ref p.UserPath, 1024);
+                                hasChanged |= ImGui.InputTextWithHint("Custom Dalamud Path", Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "XIVLauncher"), ref p.DalamudPath, 1024);
+                                hasChanged |= ImGui.InputTextWithHint("Custom Game Path", GameArguments.GamePath, ref p.GamePath, 1024);
+
+                                if (ImGui.BeginCombo("Dalamud Stream", p.SelectedDalamudStream)) {
+                                    foreach (var s in Config.DalamudStreams) {
+                                        if (ImGui.Selectable(s)) {
+                                            p.SelectedDalamudStream = s;
+                                            hasChanged = true;
+                                        }
+                                    }
+                                    ImGui.EndCombo();
+                                }
+                                
+                                
+                                var c = MutexCount();
+
+                                if (c < 2 || Config.FreeMutex) {
+                                    if (isStarting == false && ImGui.Button("Clone Session")) {
+                                        CloneSession(p);
+                                    }
+                                } else {
+                                    ImGui.Text("Max clients reached.");
+                                }
+                                
+                                ImGui.EndTabItem();
                             }
                         }
-                        ImGui.EndCombo();
-                    }
-                    
 
-                    if (hasChanged) PluginInterface.SavePluginConfig(Config);
+
+                        if (ImGui.TabItemButton($"+###profile#{i}")) {
+                            Config.Profiles.Add(new ProfileConfig());
+                            hasChanged = true;
+                        }
+                        
+                        if (!anyTabOpen && deletedItem != null) {
+                            ImGui.Text($"{deletedItem.ProfileName} Deleted.");
+                            if (ImGui.Button("Undo")) {
+                                Config.Profiles.Add(deletedItem);
+                            }
+                        }
+                        
+
+                        ImGui.EndTabBar();
+                    }
+
+
+                    if (hasChanged) Config.Save();
 
                     if (hasChanged) {
                         ClearMutex();
                     }
                     
-                    var c = MutexCount();
-
-                    if (c < 2) {
-                        if (isStarting == false && ImGui.Button("Clone Session")) {
-                            CloneSession();
-                        }
-                    } else {
-                        ImGui.Text("Max clients reached.");
-                    }
+                    
                 }
                 ImGui.End();
             }
@@ -166,7 +236,7 @@ namespace CloneSession
             if (versions == null) return string.Empty;
 
             Config.DalamudStreams = versions.Keys.ToArray();
-            PluginInterface.SavePluginConfig(Config);
+            Config.Save();
 
             if (!versions.ContainsKey(stream)) return string.Empty;
             var version = versions[stream];
@@ -186,7 +256,8 @@ namespace CloneSession
             return version.AssemblyVersion;
         }
         
-        private void CloneSession() {
+        private void CloneSession(ProfileConfig? config = null) {
+            config ??= Config.DefaultProfile;
             if (isStarting) return;
 
             ClearMutex();
@@ -203,7 +274,7 @@ namespace CloneSession
                 var existingStartInfo = (DalamudStartInfo?) getter.Invoke(null, null);
                 if (existingStartInfo == null) return;
                 
-                var dalamudPath = string.IsNullOrWhiteSpace(Config.DalamudPath) ? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "XIVLauncher") : Config.DalamudPath;
+                var dalamudPath = string.IsNullOrWhiteSpace(config.DalamudPath) ? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "XIVLauncher") : config.DalamudPath;
                 
                 
                 
@@ -214,7 +285,7 @@ namespace CloneSession
                     var assetDir = await AssetManager.EnsureAssets(new DirectoryInfo(Path.Combine(dalamudPath, "dalamudAssets")));
 
                     SimpleLog.Log("Updating Dalamud");
-                    var dalamudVersion = UpdateDalamud(Config.SelectedDalamudStream, dalamudPath);
+                    var dalamudVersion = UpdateDalamud(config.SelectedDalamudStream, dalamudPath);
 
                     if (string.IsNullOrEmpty(dalamudVersion)) {
                         SimpleLog.Log("Failed to update dalamud.");
@@ -223,7 +294,7 @@ namespace CloneSession
                     
                     
                     var parameters = string.Join(' ', new[] {
-                        "launch", "-g", $"\"{(string.IsNullOrWhiteSpace(Config.GamePath) ? GameArguments.GamePath : Config.GamePath)}\"",
+                        "launch", "-g", $"\"{(string.IsNullOrWhiteSpace(config.GamePath) ? GameArguments.GamePath : config.GamePath)}\"",
                         
                         // Dalamud Start Info
                         $"--dalamud-working-directory=\"{Path.Combine(dalamudPath,"addon","Hooks",dalamudVersion)}\"",
@@ -244,7 +315,7 @@ namespace CloneSession
                         $"language={GameArguments["language"]}",
                         $"resetConfig={GameArguments["resetConfig"]}",
                         $"ver={GameArguments["ver"]}",
-                        $"UserPath=\"{(string.IsNullOrEmpty(Config.UserPath) ? GameArguments.UserPath : Config.UserPath)}\"",
+                        $"UserPath=\"{(string.IsNullOrEmpty(config.UserPath) ? GameArguments.UserPath : config.UserPath)}\"",
                     });
                     
                     ImGui.SetClipboardText($"\"{Path.Combine(dalamudPath,"addon","Hooks",dalamudVersion, "Dalamud.Injector.exe")}\" {parameters}");
@@ -255,20 +326,15 @@ namespace CloneSession
                         var dalamudInjector = new Process() {
                             StartInfo = {
                                 FileName = Path.Combine(dalamudPath,"addon","Hooks",dalamudVersion, "Dalamud.Injector.exe"),
-                                WindowStyle = ProcessWindowStyle.Hidden,
-                                CreateNoWindow = true,
+                                WindowStyle = ProcessWindowStyle.Normal,
+                                CreateNoWindow = false,
                                 Arguments = $"{parameters}",
                                 WorkingDirectory = Path.Combine(dalamudPath,"addon","Hooks",dalamudVersion),
-                                RedirectStandardOutput = true,
-                                RedirectStandardError = true,
                             }
                         };
-                        dalamudInjector.OutputDataReceived += (sender, args) => { SimpleLog.Log(args.Data); };
+
                         dalamudInjector.Start();
-                        dalamudInjector.WaitForExit();
-                        SimpleLog.Log(dalamudInjector.StandardError.ReadToEnd());
-                        SimpleLog.Log(dalamudInjector.StandardOutput.ReadToEnd()); ;
-                        
+                        await dalamudInjector.WaitForExitAsync();
                     } catch (Exception ex) {
                         SimpleLog.Error(ex);
                     }
